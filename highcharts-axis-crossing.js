9389fde556401e31f3c02d77d051dc30a94bc6d5
David Schenck <david.schenck@outlook.com>
2020-11-08 18:54:59 +0000

    axis labels and title moved outside of plot

--- highcharts-axis-crossing.js
+++ highcharts-axis-crossing.js
@@ -7,15 +7,31 @@
  * 
  * Note
  * Unlike this plugin (https://github.com/highcharts/crossing-specific-value), 
- * you do not need to set opposite:true
+ * you do not need to set opposite:true on the X-axis
+ * and the labels and axis title should be moved outside
+ * of the plotting area.
  */
  
  (function(H) {
     H.wrap(H.Axis.prototype, 'render', function(proceed) {
         if (typeof this.options.crossing === 'number') {
-            var other = this.chart[this.isXAxis ? 'yAxis' : 'xAxis'][0];
-            this.offset = other.toPixels(this.options.crossing, true);
-            this.opposite = this.isXAxis ? true : false;
+            var other   = this.chart[this.isXAxis ? 'yAxis' : 'xAxis'][0];
+            this.offset = other.toPixels(this.options.crossing, true); 
+            
+            //get the extremes to position labels
+            var extremes = other.getExtremes()
+            var reverse = other.toPixels(extremes.min, true) - other.toPixels(this.options.crossing, true)
+
+            if(this.isXAxis){
+                this.offset += this.isXAxis ? 1 : 0; //render bugfix shift x axis by 1px 
+                this.opposite = this.isXAxis ? true : false;
+                this.options.labels.y = reverse + 15
+                this.options.title.y  = reverse + 55
+            }
+            else{
+                this.options.labels.x = reverse - 5
+                this.options.title.x  = reverse - 5
+            }
         }
         proceed.call(this);
     });
